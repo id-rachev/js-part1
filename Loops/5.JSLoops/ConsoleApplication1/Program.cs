c7f012be9282f6d819a633faa795ff4ef92b56c5
id-rachev <i.d.rachev@abv.bg>
2013-11-11 16:52:21 +0200

    Upload homeworks

---
+++ Loops/5.JSLoops/ConsoleApplication1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Dog
+    {
+        public int Age { get; set; }
+        public string Name { get; set; }
+
+        public Dog(int a, string b)
+        {
+            this.Age = a;
+            this.Name = b;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog gosho = new Dog(3, "test");
+
+        }
+    }
+}
